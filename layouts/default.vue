798bd3d956b0100b6e9432e9128679def7f57d8b
6hislain <ghislainmike80@gmail.com>
2024-11-03 09:47:04 +0200

    pina

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,12 @@
   <nav style="display: flex; gap: 10px">
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/movies">Movies</NuxtLink>
+    <NuxtLink v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
+    <a href="#" v-else @click.prevent="user.isLoggedIn = false"> Logout </a>
   </nav>
   <slot></slot>
 </template>
+
+<script setup>
+const user = useUser();
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,14 @@
 <template>
-  <form>
+  <form @submit.prevent="user.login">
     <h1>Login</h1>
     <label> username <input type="text" /> </label>
     <label> password <input type="password" /> </label>
+    <button>Login</button>
   </form>
 </template>
 
 <script setup>
+const user = useUser();
 definePageMeta({
   layout: "plain",
 });
