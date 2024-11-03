1667f2e3a165b2a0918eaa61ffcbdac3d6506209
6hislain <ghislainmike80@gmail.com>
2024-11-03 11:11:16 +0200

    nuxt img & picture

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
+    "@nuxt/image",
     ["@pinia/nuxt", { autoImports: ["defineStore", "acceptHMRUpdate"] }],
   ],
   imports: { dirs: ["stores"] },
@@ -11,4 +12,5 @@ export default defineNuxtConfig({
     "/static": { static: true },
     "/swr": { swr: true },
   },
+  image: { domains: ["m.media-amazon.com"] },
 });
